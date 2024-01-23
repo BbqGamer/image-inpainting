ce3616888388e1a22aa38ca79001de2a67083e36
BbqGamer <korba.adam@gmail.com>
2024-01-23 18:26:13 +0100

    Add option to chose model

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 import gradio as gr
 import numpy as np
-import time
 from keras.saving import load_model
 from context_encoder.model import ChannelwiseFullyConnected
 from context_encoder.train import dice_coef
-
-
-model = load_model('models/context_encoder3.h5',
-                   custom_objects={'ChannelwiseFullyConnected': ChannelwiseFullyConnected, 'dice_coef': dice_coef})
+from enum import Enum
 
 MASK_START = 96
 MASK_SIZE = 64
 MASK_END = MASK_START + MASK_SIZE
 
 
-def inpainting(image):
+models = [None, None, None, None]
+choices = [
+    'L2 MSE RMSPROP',
+    'L2 MAE ADAM',
+    'L2 MAE RMSPROP',
+    'L2 ADV'
+]
+model_files = [
+    'models/context_encoder2.h5',
+    'models/context_encoder3.h5',
+    'models/context_encoder5.h5',
+    'models/context_encoder_GAN.h5'
+]
+
+
+def load_models(choice):
+    idx = choices.index(choice)
+    if models[idx] is None:
+        models[idx] = load_model(model_files[idx],
+                                 custom_objects={
+            'ChannelwiseFullyConnected': ChannelwiseFullyConnected,
+            'dice_coef': dice_coef}
+        )
+    return models[idx]
+
+
+def inpainting(image, choice):
+    model = load_models(choice)
+
     cropped = image.copy()
     cropped[MASK_START:MASK_END, MASK_START:MASK_END, :] = 0
 
@@ -31,7 +55,8 @@ def inpainting(image):
 
 demo = gr.Interface(
     inpainting,
-    inputs=gr.Image(label="Input", type="numpy"),
+    inputs=[gr.Image(label="Input", type="numpy"),
+            gr.Dropdown(choices=choices, label="Model")],
     outputs=[gr.Image(label="Cropped"), gr.Image(label="Result")],
 )
 
